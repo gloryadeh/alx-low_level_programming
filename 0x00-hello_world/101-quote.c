037bb761065eb89699acd477b4135eb444412469
gloryadeh <glory.adeh@stu.cu.edu.ng>
2022-04-07 08:54:08 -0700

    errors

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Enry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	fprintf("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+
+	return (1);
+}

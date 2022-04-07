e43f2dfd862a760781367e1a34125cb89977ef3a
gloryadeh <glory.adeh@stu.cu.edu.ng>
2022-04-07 04:07:15 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main -Entry point
+ *
+ *
+ * Return: Alway 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+}

b0a8245ae00f9866def4992f13d07bbd7cbb9998
gloryadeh <glory.adeh@stu.cu.edu.ng>
2022-04-07 03:43:50 -0700

    betty

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 #include <string.h>
 
-int  main(){
-	
-	char str[] = "Programming is like building a multilingual puzzle";
+int  main(void)
+{
+	char str[] = "\"Programming is like building a multilingual puzzle";
+
 	puts(str);
 
-	return 0;
+	return (0);
 }

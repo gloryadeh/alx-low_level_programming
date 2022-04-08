7b074e39759b10ed92f376908513d3f97d392af5
gloryadeh <glory.adeh@stu.cu.edu.ng>
2022-04-08 13:23:13 -0700

    hex

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main -Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int hex;
+
+	for (hex = 48; hex < 58; hex++)
+	{
+		putchar(hex);
+	}
+	for (hex = 97; hex < 103; hex++)
+	{
+		putchar(hex);
+	}
+	putchar('\n');
+
+	return (0);
+}

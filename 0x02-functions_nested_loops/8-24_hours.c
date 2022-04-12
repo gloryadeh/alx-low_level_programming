8bf54516854f05a1c85c7b3fc820c75785445fd1
gloryadeh <glory.adeh@stu.cu.edu.ng>
2022-04-12 13:05:12 -0700

    24hrs

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * jack_bauer - prints time
+ *
+ * Return: Always 0 (Success)
+ */
+void jack_bauer(void)
+{
+	char a, b, c, d;
+
+	for (a = '0'; a <= '2'; a++)
+	{
+		for (b = '0'; b <= '3'; b++)
+		{
+			for (c = '0'; c <= '5'; c++)
+			{
+				for (d = '0'; d <= '9'; d++)
+				{
+					_putchar(a);
+					_putchar(b);
+					_putchar(':');
+					_putchar(c);
+					_putchar(d);
+					_putchar('\n');
+				}
+			}
+		}
+	}
+}

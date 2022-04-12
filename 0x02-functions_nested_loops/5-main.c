07a029817ec6b6ad862a4f282a74fb29df8c48e2
gloryadeh <glory.adeh@stu.cu.edu.ng>
2022-04-12 08:02:06 -0700

    lets see

--- 0x02-functions_nested_loops/5-main.c
+++ 0x02-functions_nested_loops/5-main.c
@@ -4,8 +4,9 @@ int main(void)
 {
 	int r;
 	r = print_sign(98);
-	r = print_sign(0);
-	r = print_sign(0xff);
-	r = print_sign(-1);
+	if (r < 0)
+		r *= -1;
+	_putchar(r + '0');
+	_putchar('\n');
 	return (0);
 }

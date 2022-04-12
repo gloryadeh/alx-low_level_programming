ffb1240f9d1c399a8f344c7e3bcc8bc275ee259f
gloryadeh <glory.adeh@stu.cu.edu.ng>
2022-04-12 13:53:05 -0700

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * add - sums up two integers
+ *
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: sum
+ */
+int add(int a, int b)
+{
+	int sum;
+
+	sum = a + b;
+
+	return (sum);
+}

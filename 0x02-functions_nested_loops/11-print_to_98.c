b8fc3a2b8ad7049bd807a2573e54ef56ba11cb8a
gloryadeh <glory.adeh@stu.cu.edu.ng>
2022-04-13 16:33:35 -0700

    changes

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,40 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_to_98 - prints numbers from n to 98
+ *
+ * @n: number to start printing from
+ *
+ * Return: 0
+ */
+void print_to_98(int n)
+{
+	int i;
+
+	if (n < 98)
+	{
+		for (i = n; i <= 98; i++)
+		{
+			if (i <  98)
+			{
+				printf("%d, ", i);
+			} else
+			{
+				printf("%d\n", i);
+			}
+		}
+	} else
+	{
+		for (i = n; i >= 98; i--)
+		{
+			if (i > 98)
+			{
+				printf("%d, ", i);
+			} else
+			{
+				printf("%d\n", i);
+			}
+		}
+	}
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -22,3 +22,5 @@ void jack_bauer(void);
 int add(int, int);
 
 void times_table(void);
+
+void print_to_98(int n);

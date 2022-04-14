c7a5ca603253e60916f50e4f18e816c26dabf07c
gloryadeh <glory.adeh@stu.cu.edu.ng>
2022-04-14 00:19:48 -0700

    isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isupper - checks for uppercae characters
+ * @i : The character to print
+ *
+ * Return: if success 1, else 0
+ */
+int _isupper(int i)
+{
+	if (isupper(i))
+	{
+		return (1);
+	}
+}

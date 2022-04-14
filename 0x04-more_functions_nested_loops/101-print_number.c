3ae1eee5c2cbdcfbb99641490817aed8182b0fcf
gloryadeh <glory.adeh@stu.cu.edu.ng>
2022-04-14 13:46:52 -0700

    comm

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -6,16 +6,16 @@
  */
 void print_number(int n)
 {
-	unsignes int i = n;
+	unsigned int i = n;
 
 	if (n < 0)
 	{
-		_putchar('-');
-		n = -n;
+		_putchar(45);
+		i = -i;
 	}
-	if (n / 10)
+	if (i / 10)
 	{
-		print_number(n / 10);
+		print_number(i / 10);
 	}
-	_putchar(n % 10 + '0');
+	_putchar(i % 10 + '0');
 }

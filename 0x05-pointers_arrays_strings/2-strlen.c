82fbf017b94bb51c7ad199fd640d3f0ff63d20eb
gloryadeh <gloryebikaboere@gmail.com>
2022-04-20 09:47:35 -0700

    strlen

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strlen - prints length of string
+ * @s: string
+ *
+ * Return: y
+ */
+int _strlen(char *s)
+{
+	int y;
+
+	y = strlen(s);
+
+	return (y);
+}

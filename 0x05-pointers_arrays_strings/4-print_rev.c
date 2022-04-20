5db134c0a09635a99cb71a9cfbc5f3876fe20937
gloryadeh <gloryebikaboere@gmail.com>
2022-04-20 11:05:26 -0700

    reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string in reverse
+ * @s: string
+ */
+void print_rev(char *s)
+{
+	int i, len;
+
+	len = strlen(str);
+
+	for (i = len - 1; i >= 0; i--)
+	{
+		printf("%c", str[i];
+	}
+}

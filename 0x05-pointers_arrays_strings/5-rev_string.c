0c9356578a3b648acfaded367bf7ab07f48aedda
gloryadeh <gloryebikaboere@gmail.com>
2022-04-20 16:40:49 -0700

    rev

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * rev_string - reverses a string
+ * @s: string
+ */
+void rev_string(char *s)
+{
+	int i, length;
+	char *start, *end, *temp;
+
+	len = strlen(s);
+	start = s;
+	end = s;
+
+	for (i = 0; i < len - 1; i++)
+	{
+		end++;
+	}
+	for (i = 0; i < len / 2; i++)
+	{
+		temp = *end;
+		*end = *start;
+		*start = temp;
+
+		start++;
+		end--;
+	}
+	printf("\n");
+}

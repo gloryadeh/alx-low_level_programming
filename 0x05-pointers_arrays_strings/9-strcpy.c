60394e6bed1343ca6d46aaf99492f5deccc5e710
gloryadeh <gloryebikaboere@gmail.com>
2022-04-20 13:48:27 -0700

    strcpy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strcpy - copies string from one pointer to another
+ * @dest: destination
+ * @src: source
+ *
+ * Return: copied string
+ */
+char *_strcpy(char *dest, car *src)
+{
+	return (strcpy(dest, src));
+}

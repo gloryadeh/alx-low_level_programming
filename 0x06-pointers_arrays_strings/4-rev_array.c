c5e948b8c1dc09312787870f70b543b4e40f20ab
gloryadeh <gloryebikaboere@gmail.com>
2022-04-22 03:41:53 -0700

    yessir

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * reverse_array - reverses an array of integers
+ * @a: array
+ * @n: size of array
+ */
+void reverse_array(int *a, int n)
+{
+	int i;
+	int temp[n];
+
+	for (i = 0; i < n; i++)
+	{
+		temp[n - 1 - i] = a[i];
+	}
+	for (i = 0; i < n; i++)
+	{
+		a[i] = temp[i];
+	}
+}
--- 0x06-pointers_arrays_strings/README.md
+++ 0x06-pointers_arrays_strings/README.md
@@ -3,3 +3,4 @@ This is a readmefile
 1-strncat.c  concatenates two strings.
 2-strncpy.c copies a string.
 3-strcmp.c compares two strings.
+4-rev_array reverses an array of integers

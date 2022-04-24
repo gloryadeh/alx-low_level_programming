d5550d63c74ee9fcfd2d99cef31176611edea818
Gloryadeh <66733323+gloryadeh@users.noreply.github.com>
2022-04-24 09:46:50 +0100

    Update 6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -20,7 +20,7 @@ char *cap_string(char *str)
 		{
 			if (str[i] == c[j])
 			{
-				if (str[i + 1] >= 97 && str[i + 1 ] <= 122)
+				if (str[i + 1] >= 97 && str[i + 1] <= 122)
 				{
 					str[i + 1] = str[i + 1] - 32;
 				}

c9b25f175e1638a8eb5f9f1bb8e1b96c3da64dd6
gloryadeh <gloryebikaboere@gmail.com>
2022-04-24 01:09:16 -0700

    changes

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,5 +1,5 @@
 #include "main.h"
-#include <string.h>
+#include <ctype.h>
 /**
  * cap_string - capitalizes all words a string
  * @str: string
@@ -11,15 +11,15 @@ char *cap_string(char *str)
 	char sep[] = {32, 46, 44, 59, 33, 63, 34, 40, 41, 123, 125, 9, 10};
 	int i, j;
 
-	for (i = 0; a[i] != '\0'; i++)
+	for (i = 0; str[i] != '\0'; i++)
 	{
 		for (j = 0; sep[j] != '\0'; j++)
 		{
-			if (a[i] == sep[j])
+			if (str[i] == sep[j])
 			{
-				a[i + 1] = toupper(a[i + 1]);
+				str[i + 1] = toupper(str[i + 1]);
 			}
 		}
 	}
-	return (a);
+	return (str);
 }

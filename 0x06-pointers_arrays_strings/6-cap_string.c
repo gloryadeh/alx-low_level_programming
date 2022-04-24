a21e50e66c51f05fd3e7bf782523e8e60bf487b6
gloryadeh <gloryebikaboere@gmail.com>
2022-04-24 01:01:29 -0700

    changes

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,5 +1,5 @@
 #include "main.h"
-
+#include <string.h>
 /**
  * cap_string - capitalizes all words a string
  * @str: string
@@ -8,5 +8,18 @@
  */
 char *cap_string(char *str)
 {
+	char sep[] = {32, 46, 44, 59, 33, 63, 34, 40, 41, 123, 125, 9, 10};
+	int i, j;
 
+	for (i = 0; a[i] != '\0'; i++)
+	{
+		for (j = 0; sep[j] != '\0'; j++)
+		{
+			if (a[i] == sep[j])
+			{
+				a[i + 1] = toupper(a[i + 1]);
+			}
+		}
+	}
+	return (a);
 }
--- 0x06-pointers_arrays_strings/README.md
+++ 0x06-pointers_arrays_strings/README.md
@@ -4,5 +4,5 @@ This is a readmefile
 2-strncpy.c copies a string.
 3-strcmp.c compares two strings.
 4-rev_array reverses an array of integers
-5-string_touppe.c changes lowercase of a string to uppercase.
+5-string_toupper.c changes lowercase of a string to uppercase.
 6-cap_string.c capitalizes all words of a string

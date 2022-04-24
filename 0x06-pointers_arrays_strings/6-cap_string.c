3da4c9c64699f9bf2c5387fd98876dfb7eb2e720
gloryadeh <gloryebikaboere@gmail.com>
2022-04-24 01:38:21 -0700

    okayy

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -18,9 +18,12 @@ char *cap_string(char *str)
 		}
 		for (j = 0; c[j] != '\0'; j++)
 		{
-			if (str[i] == c[j] && (str[i + 1] >= 97 && str[i + 1 ] <= 122))
+			if (str[i] == c[j])
 			{
-				str[i + 1] = str[i + 1] - 32;
+				if (str[i + 1] >= 97 && str[i + 1 ] <= 122)
+				{
+					str[i + 1] = str[i + 1] - 32;
+				}
 			}
 		}
 	}

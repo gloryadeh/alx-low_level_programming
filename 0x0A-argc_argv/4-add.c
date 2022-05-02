31e33be16da4310384710aa0eceeb85b2a342b10
Gloryadeh <66733323+gloryadeh@users.noreply.github.com>
2022-05-02 20:27:38 +0100

    Update 4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -11,21 +11,21 @@
 */
 int main(int argc, char *argv[])
 {
-  int i;
-  int sum;
-  int count;
-  
-  if (argc == 1)
-  {
-    printf("0\n");
-    return (0);
-  }
-  count = 1;
-  while (count < argc)
-  {
-    for (i = 0; argv[count][i] != '\0'; i++)
-    {
-      if (!(isdigit(argv[count][i])))
+	      int i;
+        int sum;
+        int count;
+
+        if (argc == 1)
+        {
+                printf("0\n");
+                return (0);
+        }
+        count = 1;
+        while (count < argc)
+          {
+        for (i = 0; argv[count][i] != '\0'; i++)
+        {
+            if (!(isdigit(argv[count][i])))
       {
         printf("Error\n");
         return (1);

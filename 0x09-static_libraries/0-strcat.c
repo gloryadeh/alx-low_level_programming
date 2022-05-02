99f92fbc0787515ea0c7553e17189ee7d79748fd
Gloryadeh <66733323+gloryadeh@users.noreply.github.com>
2022-05-02 12:04:41 +0100

    Update 0-strcat.c

--- 0x09-static_libraries/0-strcat.c
+++ 0x09-static_libraries/0-strcat.c
@@ -1,59 +1,24 @@
 #include "main.h"
 
 /**
+ *_strcat - concatenates  the string pointed to by @src to
+ * the end of the string pointed to by @dest
+ *@dest: String that will be appended
+ *@src: String to be concatenated upon
  *
- *  * _strcat - concatenates two strings
- *
- *   * @dest: input parameter string
- *
- *    * @src: input parameter string
- *
- *     *
- *
- *      * Return: dest
- *
- *       */
+ * Return: returns poiner to @dest
+ */
 
 char *_strcat(char *dest, char *src)
-
 {
 
-		int a;
+	int index = 0, dest_len = 0;
 
-			int b;
+	while (dest[index++])
+		dest_len++;
 
+	for (index = 0; src[index]; index++)
+		dest[dest_len++] = src[index];
 
-
-				a = 0;
-
-
-
-					while (dest[a] != 0)
-
-							{
-
-										a++;
-
-											}
-
-
-
-						b = 0;
-
-
-
-							while (src[b] != 0)
-
-									{
-
-												dest[a] = src[b];
-
-														a++;
-
-																b++;
-
-																	}
-
-								return (dest);
-
+	return (dest);
 }

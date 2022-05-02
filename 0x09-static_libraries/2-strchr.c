1f0b80a24ee097abac1d8320e30daf54865609ab
Gloryadeh <66733323+gloryadeh@users.noreply.github.com>
2022-05-02 12:18:39 +0100

    Update 2-strchr.c

--- 0x09-static_libraries/2-strchr.c
+++ 0x09-static_libraries/2-strchr.c
@@ -1,42 +1,22 @@
 #include "main.h"
 /**
+ * _strchr - locates a character in a string
+ * @s: string
+ * @c: character
  *
- *  * _strchr - locates a character in a string
+ * Return: character to end of string
  *
- *   * @s: string
- *
- *    * @c: character
- *
- *     *
- *
- *      * Return: character to end of string
- *
- *       */
-
+ */
 char *_strchr(char *s, char c)
-
 {
-
-		int i = 0;
-
-
-
-			while (s[i] >= '\0')
-
-					{
-
-								if (s[i] == c)
-
-											{
-
-															return (&s[i]);
-
-																	}
-
-										i++;
-
-											}
-
-				return (0);
-
+	int i = 0;
+	while (s[i] >= '\0')
+	{
+		if (s[i] == c)
+		{
+			return (&s[i]);
+		}
+		i++;
+	}
+	return (0);
 }

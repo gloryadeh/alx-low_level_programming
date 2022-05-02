dea7c58cc7be615e4dd1e086436b850e39b2730c
Gloryadeh <66733323+gloryadeh@users.noreply.github.com>
2022-05-02 12:27:42 +0100

    Update 3-puts.c

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,31 +1,18 @@
 #include "main.h"
 
 /**
- *
- * * _puts - print a string followed by a new line
- *
- * *@str: string to print
+ * _puts - print a string followed by a new line
+ * @str: string to print
  *
  * *Return: Nothing
- *
- * */
-
+ */
 void _puts(char *str)
-
 {
+	int i;
 
-		int i;
-
-
-
-			for (i = 0; *(str + i) != 0; i++)
-
-					{
-
-								_putchar(str[i]);
-
-									}
-
-				_putchar('\n');
-
+	for (i = 0; *(str + i) != 0; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
 }

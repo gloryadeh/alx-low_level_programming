c9649a1f8ff2564628c80ee71a72c075579abbc4
Gloryadeh <66733323+gloryadeh@users.noreply.github.com>
2022-05-02 12:40:56 +0100

    Update 5-strstr.c

--- 0x09-static_libraries/5-strstr.c
+++ 0x09-static_libraries/5-strstr.c
@@ -1,53 +1,28 @@
 #include "main.h"
 
 /**
+ * _strstr - locates a substring
+ * @haystack: string
+ * @needle: substring
  *
- *  * _strstr - locates a substring
- *
- *   * @haystack: string
- *
- *    * @needle: substring
- *
- *     *
- *
- *      * Return: 0
- *
- *       */
-
+ * Return: 0
+ */
 char *_strstr(char *haystack, char *needle)
-
 {
+	for (; *haystack != '\0'; haystack++)
+	{
+		char *l = haystack;
+		char *p = needle;
 
-		for (; *haystack != '\0'; haystack++)
-
-				{
-
-							char *l = haystack;
-
-									char *p = needle;
-
-
-
-											while (*l == *p && *p != '\0')
-
-														{
-
-																		l++;
-
-																					p++;
-
-																							}
-
-													if (*p == '\0')
-
-																{
-
-																				return (haystack);
-
-																						}
-
-														}
-
-			return (0);
-
+		while (*l == *p && *p != '\0')
+		{
+			l++;
+			p++;
+		}
+		if (*p == '\0')
+		{
+			return (haystack);
+		}
+	}
+	return (0);
 }

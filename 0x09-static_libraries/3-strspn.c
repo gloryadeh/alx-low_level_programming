a94d34218eb977d29a74a21a293e3b423c0ef3d7
Gloryadeh <66733323+gloryadeh@users.noreply.github.com>
2022-05-02 12:33:30 +0100

    Update 3-strspn.c

--- 0x09-static_libraries/3-strspn.c
+++ 0x09-static_libraries/3-strspn.c
@@ -1,69 +1,36 @@
 #include "main.h"
 
 /**
+ * _strspn - gets the egth of a prefix substring
+ * @s: string
+ * @accept: substring
  *
- *  * _strspn - gets the egth of a prefix substring
- *
- *   * @s: string
- *
- *    * @accept: substring
- *
- *     *
- *
- *      * Return: length
- *
- *       */
-
+ * Return: length
+ */
 unsigned int _strspn(char *s, char *accept)
-
 {
-
-		unsigned int res = 0;
-
-
-
-			int i, j;
-
-
-
-				i = 0;
-
-					while (s[i])
-
-							{
-
-										j = 0;
-
-												while (accept[j])
-
-															{
-
-																			if (s[i] == accept[j])
-
-																							{
-
-																												res++;
-
-																																break;
-
-																																			}
-
-																						j++;
-
-																								}
-
-														if  (s[i] != accept[j])
-
-																	{
-
-																					break;
-
-																							}
-
-																i++;
-
-																	}
-
-						return (res);
-
+	unsigned int res = 0;
+	int i, j;
+	
+	i = 0;
+	
+	while (s[i])
+	{
+		j = 0;
+		while (accept[j])
+		{
+			if (s[i] == accept[j])
+			{
+				res++;																																break;
+			}
+		j++;
+		}
+		
+		if  (s[i] != accept[j])
+		{
+			break;
+		}
+		i++;
+	}
+	return (res);
 }

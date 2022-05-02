0713995344cb6882fc5429968f666077b494013f
Gloryadeh <66733323+gloryadeh@users.noreply.github.com>
2022-05-02 12:29:43 +0100

    Update 3-strcmp.c

--- 0x09-static_libraries/3-strcmp.c
+++ 0x09-static_libraries/3-strcmp.c
@@ -1,39 +1,21 @@
 #include "main.h"
 
 /**
+ * _strcmp - compares two strings
+ * @s1: first string
+ * @s2: second string
  *
- *  * _strcmp - compares two strings
- *
- *   * @s1: first string
- *
- *    * @s2: second string
- *
- *     *
- *
- *      * Return: returns the difference
- *
- *       */
-
+ * Return: returns the difference
+ */
 int _strcmp(char *s1, char *s2)
-
 {
+	int a = 0;
+	int b = 0;
 
-		int a = 0;
-
-			int b = 0;
-
-
-
-				while (b == 0 && (s1[a] != '\0'))
-
-						{
-
-									b = s1[a] - s2[a];
-
-											a++;
-
-												}
-
-					return (b);
-
+	while (b == 0 && (s1[a] != '\0'))
+	{
+		b = s1[a] - s2[a];
+		a++;
+	}
+	return (b);
 }

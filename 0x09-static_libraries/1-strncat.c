9a08653e3a0b1854d7c5a40bc6d1d5955a9c2cfb
Gloryadeh <66733323+gloryadeh@users.noreply.github.com>
2022-05-02 12:10:13 +0100

    Update 1-strncat.c

--- 0x09-static_libraries/1-strncat.c
+++ 0x09-static_libraries/1-strncat.c
@@ -1,61 +1,22 @@
 #include "main.h"
-
 /**
- *
- *  * _strncat - concatenates two strings
- *
- *   * @dest: input parameter string
- *
- *    * @src: input parameter string
- *
- *     * @n: times to iterations
- *
- *      *
- *
- *       * Return: dest
- *
- *        */
+ *_strncat - concatenate two strings but add inputted number of bytes
+ *@dest: string to be appended upon
+ *@src: string to be completed at end of dest
+ *@n:integer parameter to compare index to
+ *Return: returns new concatenated string
+ */
 
 char *_strncat(char *dest, char *src, int n)
-
 {
 
-		int a;
+	int index = 0, dest_len = 0;
 
-			int b;
+	while (dest[index++])
+		dest_len++;
 
+	for (index = 0; src[index] && index < n; index++)
+		dest[dest_len++] = src[index];
 
-
-				a = 0;
-
-
-
-					while (dest[a] != 0)
-
-							{
-
-										a++;
-
-											}
-
-
-
-						b = 0;
-
-
-
-							while (src[b] != 0 && b < n)
-
-									{
-
-												dest[a] = src[b];
-
-														a++;
-
-																b++;
-
-																	}
-
-								return (dest);
-
+	return (dest);
 }

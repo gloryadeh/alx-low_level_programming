5403df8a76da211812febd36302ae820d2e57ecd
Gloryadeh <66733323+gloryadeh@users.noreply.github.com>
2022-05-02 12:24:50 +0100

    Update 2-strncpy.c

--- 0x09-static_libraries/2-strncpy.c
+++ 0x09-static_libraries/2-strncpy.c
@@ -1,45 +1,24 @@
 #include "main.h"
 
 /**
+ * _strncpy - copies a string
+ * @dest: destination
+ * @src: source
+ * @n: number of bytes
  *
- *  * _strncpy - copies a string
- *
- *   * @dest: destination
- *
- *    * @src: source
- *
- *     * @n: number of bytes
- *
- *      *
- *
- *       * Return: dest
- *
- *        */
-
+ * Return: dest
+ */
 char *_strncpy(char *dest, char *src, int n)
-
 {
-
-		int i;
-
-
-
-			for (i = 0; i < n && src[i] != '\0'; i++)
-
-					{
-
-								dest[i] = src[i];
-
-									}
-
-				for (; i < n; i++)
-
-						{
-
-									dest[i] = '\0';
-
-										}
-
-					return (dest);
-
+	int i;
+	
+	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+	for (; i < n; i++)
+	{
+		dest[i] = '\0';
+	}
+	return (dest);
 }

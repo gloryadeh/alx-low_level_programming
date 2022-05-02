fa5744b50955e8e38e334409273843eb93c96c53
Gloryadeh <66733323+gloryadeh@users.noreply.github.com>
2022-05-02 21:27:37 +0100

    Update 100-change.c

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -9,10 +9,8 @@
  */
 int main(int argc, char *argv[])
 {
-	/*Declaring variables*/
 	int position, total, change, aux;
-	int coins[] = {25, 10, 5, 2, 1, '\0'}; /*Array int*/
-
+	int coins[] = {25, 10, 5, 2, 1, '\0'};
 	position = total = change = aux = 0;
 
 	if (argc != 2)
@@ -20,33 +18,22 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		return (1);
 	}
-
-	total = atoi(argv[1]); /*Covert str to int*/
-
+	total = atoi(argv[1]);
 	if (total <= 0)
 	{
 		printf("0\n");
 		return (0);
 	}
-
-	/*Declaring While*/
 	while (coins[position] != '\0')
 	{
-		/* if the total is greater than the coin at the current index */
 		if (total >= coins[position])
 		{
-			/* divide the total by the coin */
 			aux = (total / coins[position]);
-			/* add the number of coins needed */
 			change += aux;
-			/* subtract the change from total */
 			total -= (coins[position] * aux);
 		}
-
 		position++;
-
 	}
-
 	printf("%d\n", change);
 	return (0);
 }

472edc521816024c6a5d07b6c79c0899059f5210
gloryadeh <glory.adeh@stu.cu.edu.ng>
2022-05-05 06:13:16 -0700

    create array

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * create_array - creates an array
+ * @size: size of array
+ * @c: initializing char
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *array;
+	unsigned int i;
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+
+	array = malloc(sizeof(char) * size);
+
+	/* check if malloc was successful */
+
+	if (array == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < size; i++)
+	{
+		array[i] = c;
+	}
+	return (array);
+}

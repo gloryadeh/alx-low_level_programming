b789c5e84462c2320ac6a38615486f1ce5297606
gloryadeh <glory.adeh@stu.cu.edu.ng>
2022-05-09 14:03:43 -0700

    sheesh

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -11,7 +11,7 @@
 int *array_range(int min, int max)
 {
 	int i, size;
-	char *s;
+	int *s;
 
 	if (min > max)
 		return (NULL);
@@ -21,7 +21,7 @@ int *array_range(int min, int max)
 		return (NULL);
 	for (i = 0; i < size; i++, min++)
 	{
-		a[i] = min;
+		s[i] = min;
 	}
 	return (s);
 }

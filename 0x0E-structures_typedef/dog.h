fdc470952b1b9673308f564d7f034e98a6149d07
gloryadeh <glory.adeh@stu.cu.edu.ng>
2022-05-11 05:37:32 -0700

    dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef MACRO_H
+#define MACRO_H
+
+/**
+ * struct dog - Dog info
+ * @name: name of dog
+ * @age: dog's age
+ * @owner: owner of dog
+ *
+ * Description: Structure with dog info
+ */
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} dog;
+
+#endif

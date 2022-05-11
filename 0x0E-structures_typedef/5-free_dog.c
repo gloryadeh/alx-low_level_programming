ce38f37ebdfbbae4e56492e4bbb291db6a0eeab0
gloryadeh <glory.adeh@stu.cu.edu.ng>
2022-05-11 14:54:58 -0700

    free

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,1 +1,18 @@
-nc
+#include "dog.h"
+#include <stdio.h>
+
+/**
+ * free_dog - frees dogs
+ * @d: pointer to dog_t
+ *
+ * Return: nothing
+ */
+void free_dog(dog_t *d)
+{
+	if (d != NULL)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
+}

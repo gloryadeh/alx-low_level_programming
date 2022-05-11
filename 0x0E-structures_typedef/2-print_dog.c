51d56ef37172dd04ad6f468d9dba7968276ba2cf
gloryadeh <glory.adeh@stu.cu.edu.ng>
2022-05-11 06:03:48 -0700

    print

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -21,18 +21,12 @@ void print_dog(struct dog *d)
 	{
 		printf("Name: %s\n", d->name);
 	}
-	if (d->age == NULL)
-	{
-		printf("Age: (nil)\n");
-	} else
-	{
-		printf("Age: %f\n", d->age);
-	}
+	printf("Age: %f\n", d->age);
 	if (d->owner == NULL)
 	{
-		printd("Owner: (nil)\n");
+		printf("Owner: (nil)\n");
 	} else
 	{
-		printf("Owner: %s\n");
+		printf("Owner: %s\n0", d->owner);
 	}
 }

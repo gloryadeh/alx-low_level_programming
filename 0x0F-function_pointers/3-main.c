7625589ef78a28adada7dfad0d5d019b62c968f9
gloryadeh <glory.adeh@stu.cu.edu.ng>
2022-05-13 16:34:14 -0700

    sheesh

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -11,9 +11,8 @@
  */
 int main(int argc, char *argv[])
 {
-	int arg1, arg2, result;
-	char o;
-	int (*func)(int, int);
+	int arg1, arg2;
+	int a, b, c, d, e;
 
 	if (argc != 4)
 	{
@@ -23,24 +22,24 @@ int main(int argc, char *argv[])
 	arg1 = atoi(argv[1]);
 	arg2 = atoi(argv[3]);
 
-	func = get_op_func(argv[2]);
+	a = strcmp(argv[2], "+");
+	b = strcmp(argv[2], "-");
+	c = strcmp(argv[2], "*");
+	d = strcmp(argv[2], "/");
+	e = strcmp(argv[2], "%");
 
-	if (!func)
+	if (a && b && c && d && e)
 	{
 		printf("Error\n");
 		exit(99);
 	}
-
-	o = *argv[2];
-
-	if ((o == '/' || o == '%') && arg2 == 0)
+	if ((d == 0 || e == 0) && arg2 == 0)
 	{
 		printf("Error\n");
 		exit(100);
 	}
 
-	result = func(arg1, arg2);
-	printf("%d\n", result);
+	printf("%d\n", (*get_op_func(argv[2]))(arg1, arg2));
 
 	return (0);
 }

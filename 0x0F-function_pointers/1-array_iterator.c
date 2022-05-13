5a30ec2712995571e2744a345284028e3dd59e79
gloryadeh <glory.adeh@stu.cu.edu.ng>
2022-05-13 12:14:47 -0700

    array_iterator

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -15,11 +15,8 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 
 	if (array == NULL || action == NULL)
 		return;
-	else
+	for (i = 0; i < size; i++)
 	{
-		for (i = 0; i < size; i++)
-		{
-			action(array[i]);
-		}
+		action(array[i]);
 	}
 }

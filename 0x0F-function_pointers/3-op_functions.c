98881ff914e742081acc6b2a6202d2b3adb5a0f6
gloryadeh <glory.adeh@stu.cu.edu.ng>
2022-05-13 11:47:57 -0700

    operations

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,56 @@
+#include "3-calc.h"
+
+/**
+ * op_add - adds two numbers
+ * @a: int a
+ * @b: int b
+ * Return: sum of numbers
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - subtracts two numbers
+ * @a: int a
+ * @b: int b
+ * Return: difference of numbers
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - multiplies two numbers
+ * @a: int a
+ * @b: int b
+ * Return: product
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - divides two numbers
+ * @a: int a
+ * @b: int b
+ * Return: division
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - modulus
+ * @a: int a
+ * @b: int b
+ * Return: remainder of division
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}

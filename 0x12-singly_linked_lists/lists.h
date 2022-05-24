b2c74d2d783e94263a6690b79af03c302abafce1
gloryadeh <glory.adeh@stu.cu.edu.ng>
2022-05-24 16:51:11 +0100

    Print elements in list

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,8 +1,7 @@
 #ifndef LISTS_H
 #define LISTS_H
 
-#include <stddef.h>
-#include <stdio.h>
+#include <stdlib.h>
 
 /**
  * struct list_s - singly linked list
@@ -19,7 +18,6 @@ typedef struct list_s
     struct list_s *next;
 } list_t;
 
-int _putchar(char c);
 size_t print_list(const list_t *h);
 
 #endif

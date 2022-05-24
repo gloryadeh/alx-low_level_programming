586776d770ff18f3da773c4c65ab0999369e1eda
gloryadeh <glory.adeh@stu.cu.edu.ng>
2022-05-24 16:42:53 +0100

    Print elements in list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * print_list - prints the elements in a list
+ * @h: pointer to list
+ *
+ * Return: number of elements
+ */
+size_t print_list(const list_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+		{
+			printf("[0] %s\n", "nil");
+		}
+		else
+		{
+			printf("[%d] %s\n", h->len, h->str);
+		}
+		h = h->next;
+		count++;
+	}
+	return (count);
+}
---
+++ 0x12-singly_linked_lists/README.md
@@ -0,0 +1,2 @@
+This is a readme file
+0-prints_lists prints all elements of a list

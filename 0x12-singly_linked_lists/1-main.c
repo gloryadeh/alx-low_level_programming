0b211b752fa9b9e0d8d93d5bdadbe07fd7d76de1
gloryadeh <glory.adeh@stu.cu.edu.ng>
2022-05-24 17:14:16 +0100

    Print number of elements

--- 0x12-singly_linked_lists/1-main.c
+++ 0x12-singly_linked_lists/1-main.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <string.h>
+#include <stdio.h>
 #include "lists.h"
 
 /**
@@ -25,15 +26,9 @@ int main(void)
     new->len = 5;
     new->next = head;
     head = new;
-    n = print_list(head);
+    n = list_len(head);
     printf("-> %lu elements\n", n);
-
-    printf("\n");
     free(new->str);
-    new->str = NULL;
-    n = print_list(head);
-    printf("-> %lu elements\n", n);
-
     free(new);
     return (0);
 }
--- 0x12-singly_linked_lists/README.md
+++ 0x12-singly_linked_lists/README.md
@@ -1,2 +1,3 @@
 This is a readme file
 0-prints_lists prints all elements of a list
+1-list_len returns number of elements in a linked list

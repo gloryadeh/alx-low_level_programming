2424c23aed8b3de439494289b6b06ae6e23ef617
gloryadeh <glory.adeh@stu.cu.edu.ng>
2022-05-25 00:34:48 +0100

    Fix tast 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -18,13 +18,14 @@ size_t print_list(const list_t *h)
 	{
 		if (h->str == NULL)
 		{
-			printf("[%d] %s\n", 0, "nil");
+			printf("[%u] %s\n", 0, "nil");
 		}
 		else
 		{
-			printf("[%d] %s\n", h->len, h->str);
+			printf("[%u] %s\n", h->len, h->str);
 		}
 		h = h->next;
 	}
+	printf("[%u] %s\n", h->len, h->str);
 	return (count);
 }

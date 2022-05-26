f09c756b2d9f197c5a6bfbcfb97f8bc2a314947a
gloryadeh <glory.adeh@stu.cu.edu.ng>
2022-05-26 21:36:32 +0100

    Task 10

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,38 @@
+#include "lists.h"
+
+/**
+ * delete_nodeint_at_index - deletes the node at index
+ * @head: pointer to head
+ * @index: index of node that should be deleted
+ * Return: 1 if success, else -1
+ */
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	unsigned in i;
+	listint_t *currptr, *node;
+
+	currptr = *head;
+	if (head == NULL || *head == NULL)
+		return (-1);
+	for (i = 0; i < index - 1 && tmp != NULL && index != 0; i++)
+	{
+		currptr = currptr->next;
+	}
+	if (tmp == NULL)
+		return (NULL);
+	if (index == 0)
+	{
+		node = currptr->next;
+		free(currptr);
+		*head = node;
+	} else
+	{
+		if (currptr->next == NULL)
+			node = currptr->next;
+		else
+			node = currptr->next->next;
+		free(currptr->next);
+		currptr->next = node;
+	}
+	return (1);
+}
--- 0x13-more_singly_linked_lists/README.md
+++ 0x13-more_singly_linked_lists/README.md
@@ -7,3 +7,5 @@
 6-pop_listint.c deletes the head node of a list and returns the head nodes data
 7-get_nodeint.c returns the nth node of a list
 8-sum_listint.c returns the sum of all the data of a list
+9-insert_nodeint.c inserts a node at an index of the list
+10-delete_nodeint.code deletes a node at an index in the list

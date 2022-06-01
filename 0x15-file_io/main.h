27826c59416cb87fe5b1c3ce300307ea84cc0736
gloryadeh <glory.adeh@stu.cu.edu.ng>
2022-06-01 15:30:01 +0100

    Fis issues

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,17 +1,27 @@
 #ifndef FILE_H
 #define FILE_H
 
+/* open */
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
 
+/* read and write*/
 #include <unistd.h>
+
+/* dprintf and printf */
 #include <stdio.h>
+
+/* malloc */
 #include <stdlib.h>
 
+#define BUFF_SIZE 1024
+
+/* created functions */
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
-unsigned int _strlen(char *s);
 int append_text_to_file(const char *filename, char *text_content);
 
+/* helper functions */
+unsigned int _strlen(char *s);
 #endif

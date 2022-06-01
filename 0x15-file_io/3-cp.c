cc301d8028cd84cf4346c251152380fad8cef9bd
gloryadeh <glory.adeh@stu.cu.edu.ng>
2022-06-01 15:44:22 +0100

    Fis issues

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -11,19 +11,15 @@ int main(int ac, char **av)
 {
 	char buff[1024];
 	int fd_to, fd_from, num_read, num_write;
-	mode_t mode;
 
 	if (ac != 3)
-	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
-	}
 	fd_from = open(av[1], O_RDONLY);
 	if (fd_from == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[1]), exit(98);
 	}
-	mode = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH;
-	fd_to = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, mode);
+	fd_to = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, 664);
 	if (fd_to == -1)
 	{
 		close(fd_from);
